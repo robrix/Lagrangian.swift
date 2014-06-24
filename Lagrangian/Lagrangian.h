ff3801c38fb6ceac3ad89fa9d3638ed193c34802
Rob Rix <rob.rix@github.com>
2014-06-23 21:09:28 -0400

    Tidy up the header.

--- Lagrangian/Lagrangian.h
+++ Lagrangian/Lagrangian.h
@@ -1,19 +1,8 @@
-//
-//  Lagrangian.h
-//  Lagrangian
-//
-//  Created by Rob Rix on 2014-06-23.
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
-//
 
-#import <Cocoa/Cocoa.h>
+/// Project version number for Lagrangian.
+extern double LagrangianVersionNumber;
 
-//! Project version number for Lagrangian.
-FOUNDATION_EXPORT double LagrangianVersionNumber;
-
-//! Project version string for Lagrangian.
-FOUNDATION_EXPORT const unsigned char LagrangianVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Lagrangian/PublicHeader.h>
-
+/// Project version string for Lagrangian.
+extern const unsigned char LagrangianVersionString[];
 
