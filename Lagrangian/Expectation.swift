00ec27c32d3b2b46c7d79b972ec3b9547994bc94
Rob Rix <rob.rix@github.com>
2014-06-24 10:27:06 -0400

    Expectations are equatable, in an ugly way.

--- Lagrangian/Expectation.swift
+++ Lagrangian/Expectation.swift
@@ -12,6 +12,13 @@ struct Expectation<T, H : Hypothesis where H.SubjectType == T> {
 }
 
 
+func == <T, H : Hypothesis where H.SubjectType == T> (a: Expectation<T, H>, b: Expectation<T, H>) -> Bool {
+	return a.description == b.description
+}
+
+extension Expectation : Equatable {}
+
+
 /// Constructs an \c Expectation that a \c Subject will equal some other value.
 func == <T : Equatable> (subject: Subject<T>, object: T) -> Expectation<T, Equal<T>> {
 	return Expectation(subject: subject, hypothesis: Equal(object: object))
