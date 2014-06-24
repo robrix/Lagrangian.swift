b3e0592f5866e1ec25a82887a4d39af02072957e
Rob Rix <rob.rix@github.com>
2014-06-23 21:48:50 -0400

    Hypotheses are Printable.

--- Lagrangian/Hypothesis.swift
+++ Lagrangian/Hypothesis.swift
@@ -1,7 +1,7 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 /// A testable hypothesis about a subject.
-protocol Hypothesis {
+protocol Hypothesis : Printable {
 	typealias SubjectType
 	
 	/// Test whether the hypothesis is valid for a given subject.
@@ -13,6 +13,8 @@ protocol Hypothesis {
 struct Not<T, H : Hypothesis where H.SubjectType == T> : Hypothesis {
 	let hypothesis: H
 	
+	var description: String { return "not \(hypothesis.description)" }
+	
 	func test(subject: T) -> Bool {
 		return hypothesis.test(subject)
 	}
@@ -22,6 +24,8 @@ struct Not<T, H : Hypothesis where H.SubjectType == T> : Hypothesis {
 /// A hypothesis that the subject equals the object.
 struct Equal<T : Equatable> : Hypothesis {
 	let object: T
+	
+	var description: String { return "equal \(object)" }
 	
 	func test(subject: T) -> Bool {
 		return subject == object
