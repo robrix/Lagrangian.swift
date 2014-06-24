c6cd7eca436e35e0188e044377a03abd77016ef4
Rob Rix <rob.rix@github.com>
2014-06-24 11:11:24 -0400

    Clean up the tests.

--- LagrangianTests/LagrangianTests.swift
+++ LagrangianTests/LagrangianTests.swift
@@ -1,35 +1,17 @@
-//
-//  LagrangianTests.swift
-//  LagrangianTests
-//
-//  Created by Rob Rix on 2014-06-23.
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
-//
 
 import XCTest
+import Lagrangian
 
 class LagrangianTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testPrintable() {
+		let expectations = [
+			(%"string" == "string", "string should equal string"),
+			(%2 == 23, "2 should equal 23"),
+//			(%1 == 1, "1 should equal 1"),
+		]
+		for (expectation, description) in expectations {
+			XCTAssertEqual(expectation.description, description, "")
+		}
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
