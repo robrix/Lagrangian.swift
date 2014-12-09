ec2d55128fb590759547bff419ee7162ee8e9f9c
Rob Rix <rob.rix@github.com>
2014-12-09 14:29:28 -0500

    There’s a lot of stuff here that I don’t remember.

--- Lagrangian/Expectation.swift
+++ Lagrangian/Expectation.swift
@@ -3,63 +3,53 @@
 let _expectationTest = suite("Expectation")
 
 /// An expectation is the combination of a subject & hypothesis.
-public struct Expectation<T, H : HypothesisType where H.Subject == T> {
-	private var subject: Subject<T>
+public final class Expectation<H : HypothesisType> : Printable, Hashable {
+	private var subject: Subject<H.Subject>
 	private let hypothesis: H
 
-	public init(subject: Subject<T>, hypothesis: H) {
+	public init(_ subject: Subject<H.Subject>, _ hypothesis: H) {
 		self.subject = subject
 		self.hypothesis = hypothesis
 	}
-}
 
-
-extension Expectation : Printable {
 	public var description: String {
 		return "\(subject) should \(hypothesis)"
 	}
-}
 
-
-public func == <T, H : HypothesisType where H.Subject == T> (a: Expectation<T, H>, b: Expectation<T, H>) -> Bool {
-	return a.description == b.description
-}
-
-extension Expectation : Equatable {}
-
-
-extension Expectation : Hashable {
 	public var hashValue: Int {
 		return description.hashValue
 	}
 }
 
+public func == <H : HypothesisType> (a: Expectation<H>, b: Expectation<H>) -> Bool {
+	return a.description == b.description
+}
+
 /// fixme: file a radar about gensyms
 /// fixme: file a radar about metaprogramming to define constants
 /// fixme: file a radar about strict globals/constructor funcs
-let _t0 =
-	given([Int]())
-	.when { x in x.append(1) ; return x }
-	.expect { (x: Subject<[Int]>) in x.value.count == x.value.count + 1 }
+//private let _t0 =
+//	given([Int]())
+//	.when { x in x.append(1) ; return x }
+//	.expect { (x: Subject<[Int]>) in
+////		x.map { $0.count } == 1
+//		x.value.count == 1
+//	}
 //	.expect { x, y in x[x.count - 1] == y }
-//	%2 == 23
-//	%"" == ""
-
 
 
 /// Constructs an \c Expectation that a \c Subject will equal some other value.
-public func == <T : Equatable> (subject: Subject<T>, object: T) -> Expectation<T, Equal<T>> {
-	return Expectation(subject: subject, hypothesis: Equal(object: object))
+public func == <T : Equatable> (subject: Subject<T>, object: T) -> Expectation<Equal<T>> {
+	return Expectation(subject, Equal(object))
 }
 
-
 /// Constructs an \c Expectation that a \c Subject will not equal some other value.
-public func != <T : Equatable> (subject: Subject<T>, object: T) -> Expectation<T, Not<Equal<T>>> {
-	return Expectation(subject: subject, hypothesis: Not(hypothesis: Equal(object: object)))
+public func != <T : Equatable> (subject: Subject<T>, object: T) -> Expectation<Not<Equal<T>>> {
+	return Expectation(subject, Not(Equal(object)))
 }
 
 
 /// Negates a hypothesis.
 public prefix func ! <H : HypothesisType> (hypothesis: H) -> Not<H> {
-	return Not(hypothesis: hypothesis)
+	return Not(hypothesis)
 }
