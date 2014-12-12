ccb0a4d371da5796c1ff7ddb52ace5a85fab6fec
Rob Rix <rob.rix@github.com>
2014-12-12 12:02:48 -0500

    `any` is in Madness now.

--- Lagrangian/Demangling.swift
+++ Lagrangian/Demangling.swift
@@ -6,10 +6,6 @@ let marker = ignore("_T")
 
 let count = (%("0"..."9"))+ --> { strtol("".join($0), nil, 10) }
 
-// todo: this belongs in Madness
-let any: Parser<String>.Function = { x in (x[x.startIndex..<advance(x.startIndex, 1, x.endIndex)], x[advance(x.startIndex, 1, x.endIndex)..<x.endIndex]) }
-
-let three = any * 3
 let many: Int -> Parser<String>.Function = { n in any * n --> { "".join($0) } }
 
 public let identifier: Parser<String>.Function = { count($0).map { many($0)($1) } ?? nil }
