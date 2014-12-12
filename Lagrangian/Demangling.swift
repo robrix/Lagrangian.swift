230abdd3cced5c7dd3f465726a35224e8e3629e1
Rob Rix <rob.rix@github.com>
2014-12-11 22:21:40 -0500

    Parsers section.

--- Lagrangian/Demangling.swift
+++ Lagrangian/Demangling.swift
@@ -1,5 +1,7 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+// MARK: - Parsers
+
 let marker = ignore("_T")
 
 let count = (%("0"..."9"))+ --> { strtol("".join($0), nil, 10) }
@@ -10,9 +12,7 @@ let any: Parser<String>.Function = { x in (x[x.startIndex..<advance(x.startIndex
 let three = any * 3
 let many: Int -> Parser<String>.Function = { n in any * n --> { "".join($0) } }
 
-public let identifier: Parser<String>.Function = {
-	count($0).map { many($0)($1) } ?? nil
-}
+public let identifier: Parser<String>.Function = { count($0).map { many($0)($1) } ?? nil }
 
 public let mangled = marker ++ identifier+ --> { ".".join($0) }
 
