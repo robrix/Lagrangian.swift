3bfcd1164c4434b23d69dbc170ef39652cf229f3
Rob Rix <rob.rix@github.com>
2014-12-12 23:24:10 -0500

    Some more assertion functions.

--- LagrangianTests/DemanglingTests.swift
+++ LagrangianTests/DemanglingTests.swift
@@ -15,4 +15,25 @@ final class DemanglingTests: XCTestCase {
 		let (e, a) = (expected(), actual())
 		if e != a { XCTFail("\(a) is not equal to \(e)", file: file, line: line) }
 	}
+
+	func assertNotEqual<T: Equatable>(actual: @autoclosure () -> T?, _ unexpected: @autoclosure () -> T?, _ file: String = __FILE__, line: UInt = __LINE__) {
+		let (e, a) = (unexpected(), actual())
+		if e == a { XCTFail("\(a) is equal to \(e)", file: file, line: line) }
+	}
+
+	func assertNotNil<T>(actual: @autoclosure () -> T?, _ file: String = __FILE__, _ line: UInt = __LINE__) -> T? {
+		return actual().map {
+			$0
+		} ?? failure("unexpected nil", file: file, line: line)
+	}
+
+	func failure(message: String, file: String = __FILE__, line: UInt = __LINE__) -> Bool {
+		XCTFail(message, file: file, line: line)
+		return false
+	}
+
+	func failure<T>(message: String, file: String = __FILE__, line: UInt = __LINE__) -> T? {
+		XCTFail(message, file: file, line: line)
+		return nil
+	}
 }
